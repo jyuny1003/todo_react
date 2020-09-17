c62ce4c560f87f4e7b1cc5e644522f0b463f8e76
jyuny1003 <dlwlgus299@naver.com>
2020-09-17 18:29:34 +0900

    클릭이벤트 한개로 통합

--- todo/src/Fruits.js
+++ todo/src/Fruits.js
@@ -3,26 +3,34 @@ import { Button } from 'antd';
 
 
 function Fruits() {
-    const [name, setName] = React.useState('')
+    // const [name, setName] = React.useState('')
 
-    const click1 = () => {
-        setName('바나나')
+    // const click1 = () => {
+    //     setName('바나나')
+    // }
+    // const click2 = () => {
+    //     setName('사과')
+    // }
+    // const click3 = () => {
+    //     setName('딸기')
+    // }
+
+    const [state, setState] = React.useState('')
+
+    const change = (e) => {     
+        console.log(e.target)   
+        setState(e.target.innerHTML)
     }
-    const click2 = () => {
-        setName('사과')
-    }
-    const click3 = () => {
-        setName('딸기')
-    }
+
 
     return (
         
             <div>
-                <div>{name}</div>
+                <div>{state}</div>
                 <div>
-                    <span><Button type="primary" onClick={click1}>바나나</Button></span>
-                    <span><Button type="primary" onClick={click2}>사과</Button></span>
-                    <span><Button type="primary" onClick={click3}>딸기</Button></span>
+                    <span><Button type="primary" onClick={change}>바나나</Button></span>
+                    <span><Button type="primary" onClick={change}>사과</Button></span>
+                    <span><Button type="primary" onClick={change}>딸기</Button></span>
                 </div>
             </div>
     )
